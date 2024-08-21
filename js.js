44b6a91b4d5cf82aae1d00d76010a94c56cabeb5
MdHasanAliGHUN <mdhassanali01947@gmail.com>
2024-08-21 10:54:00 -0700

    Navigation is Complited

---
+++ js.js
@@ -0,0 +1,14 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                'btn-bg-color': '#ABEF5F',
+                'bg-color': "#FFFFFF"
+            },
+            fontFamily:{
+                'manrope': "Manrope"
+
+            }
+        },
+    },
+}
